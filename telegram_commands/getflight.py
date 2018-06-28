7c34d26cde4f21bd9395395a0ec8dc9200b46da0
SalamiArmy <ultrakiff@gmail.com>
2018-06-28 22:15:15 +0200

    pulling missing update from info boet

--- telegram_commands/getflight.py
+++ telegram_commands/getflight.py
@@ -1,26 +1,35 @@
 # coding=utf-8
-import json
 import urllib
 
-import telegram
 from bs4 import BeautifulSoup
 
 
 def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     requestText = message.replace(bot.name, "").strip()
-    airportCode, error = get_airport_code(requestText)
-    if airportCode != 'No matching entries found...':
-        bot.sendMessage(chat_id=chat_id, text=airportCode)
+    if (requestText[3] == " " and requestText[7] == " " and requestText[12] == "-" and requestText[15] == "-" and len(requestText) > 19 and requestText[18] == " " and requestText[23] == "-" and requestText[26] == "-"):
+        bot.sendMessage(chat_id=chat_id, text=get_returnflights(requestText), disable_web_page_preview=True)
         return True
     else:
-        if error:
-            bot.sendMessage(chat_id=chat_id,
-                            text='I\'m sorry ' + (user if not user == '' else 'Dave') +
-                                 error)
+        if (requestText[3] == " " and requestText[7] == " " and requestText[12] == "-" and requestText[15] == "-"):
+            bot.sendMessage(chat_id=chat_id, text=get_flights(requestText), disable_web_page_preview=True)
+            return True
         else:
-            bot.sendMessage(chat_id=chat_id,
-                            text='I\'m sorry ' + (user if not user == '' else 'Dave') +
-                                 ', I\'m afraid I can\'t quite place ' + requestText.encode('utf-8') + '.')
+            airportCode, error = get_airport_code(requestText)
+            if airportCode != '':
+                bot.sendMessage(chat_id=chat_id, text=airportCode)
+                return True
+            else:
+                if error:
+                    bot.sendMessage(chat_id=chat_id,
+                                    text='I\'m sorry ' + (user if not user == '' else 'Dave') + ', ' +
+                                         error)
+                else:
+                    bot.sendMessage(chat_id=chat_id,
+                                    text='I\'m sorry ' + (user if not user == '' else 'Dave') +
+                                         ', I\'m afraid I can\'t quite place ' + requestText.encode('utf-8') + '.')
+    bot.sendMessage(chat_id=chat_id,
+                    text='This bot has three modes:\nAirport Code Search: for example \'/getflight lucerne\' request returns \'QLJ\'.\nOne-Way Search: for example \'/getflight DUR DUB 1988-06-28\' request returns flights from DUR to DUB on 1988-06-28.\nRound-Trip Search: for example \'\getflight DUR DUB 1988-06-28 1988-07-17\' request returns flights with a return flight on 1988-07-17.')
+
 
 
 def get_airport_code(cityName):
@@ -30,5 +39,20 @@ def get_airport_code(cityName):
     data = BeautifulSoup(code, 'html.parser')
     error = data.find('b').string
     rawAirportCode = str(data.findAll('b')[1]) if error != 'No matching entries found...' else ''
-    airportCode = rawAirportCode[4:rawAirportCode.index(')<br>')] if len(rawAirportCode) > 15 else ''
+    if rawAirportCode != '':
+        if rawAirportCode.replace('<b>', '').replace('</b>', '') != cityName:
+            airportCode = rawAirportCode[4:rawAirportCode.index(')<br/>')] if len(rawAirportCode) > 13 else ''
+        else:
+            airportCode = str(data.findAll('center')[0])\
+                .replace('<center>\n<b>Airport list:</b><br/>\n\t\t\n\t\t\t', '')\
+                .replace('<br/>\n\t\t\t', '\n')\
+                .replace('<br/>\n<br/><br/>\n<!-- /tomany -->\n<!-- nomatch -->\n<p align="CENTER"><b><a href="index.php">Search again?</a></b></p>\n</center>', '')
     return airportCode, error.replace('Here are the results of your search:', '')
+
+def get_flights(requestText):
+    return 'https://www.google.co.uk/flights/?gl=ie#flt=' + requestText.replace(' ', '.') + ';c:EUR;e:1;sd:1;t:f;tt:o'
+
+def get_returnflights(requestText):
+    requestParts = requestText.split(' ')
+    parsedRequest = requestParts[0] + '.' + requestParts[1] + '.' + requestParts[2] + '*' + requestParts[1] + '.' + requestParts[0] + '.' + requestParts[3]
+    return 'https://www.google.co.uk/flights/?gl=ie#flt=' + parsedRequest + ';c:EUR;e:1;sd:1;t:f'
--- telegram_tests/test_getflight.py
+++ telegram_tests/test_getflight.py
@@ -7,12 +7,35 @@ import telegram_commands.getflight as getflight
 
 
 class TestPlace(unittest.TestCase):
-    def test_place(self):
-        requestText = 'lucerne'
+    def test_getairport(self):
+        requestText = 'belfast'
 
         keyConfig = ConfigParser.ConfigParser()
         keyConfig.read(["keys.ini", "..\keys.ini"])
-        bot = telegram.Bot(keyConfig.get('Telegram', 'TELE_BOT_ID'))
-        chatId = keyConfig.get('BotAdministration', 'TESTING_PRIVATE_CHAT_ID')
+        keyConfig.read(["bot_keys.ini", "..\\bot_keys.ini"])
+        bot = telegram.Bot(keyConfig.get('BotIDs', 'TELEGRAM_BOT_ID'))
+        chatId = keyConfig.get('BotAdministration', 'TESTING_TELEGRAM_PRIVATE_CHAT_ID')
+
+        getflight.run(bot, chatId, 'Admin', keyConfig, requestText)
+
+    def test_getonewayflight(self):
+        requestText = 'DUR DUB 2018-08-15'
+
+        keyConfig = ConfigParser.ConfigParser()
+        keyConfig.read(["keys.ini", "..\keys.ini"])
+        keyConfig.read(["bot_keys.ini", "..\\bot_keys.ini"])
+        bot = telegram.Bot(keyConfig.get('BotIDs', 'TELEGRAM_BOT_ID'))
+        chatId = keyConfig.get('BotAdministration', 'TESTING_TELEGRAM_PRIVATE_CHAT_ID')
+
+        getflight.run(bot, chatId, 'Admin', keyConfig, requestText)
+
+    def test_getreturnflight(self):
+        requestText = 'DUR DUB 2019-04-14 2019-04-16'
+
+        keyConfig = ConfigParser.ConfigParser()
+        keyConfig.read(["keys.ini", "..\keys.ini"])
+        keyConfig.read(["bot_keys.ini", "..\\bot_keys.ini"])
+        bot = telegram.Bot(keyConfig.get('BotIDs', 'TELEGRAM_BOT_ID'))
+        chatId = keyConfig.get('BotAdministration', 'TESTING_TELEGRAM_PRIVATE_CHAT_ID')
 
         getflight.run(bot, chatId, 'Admin', keyConfig, requestText)
